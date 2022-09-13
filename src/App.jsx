97a061a4c277edbb08b13f58b31bad21f0e3f7cf
Edswel <franckini@yahoo.com>
2022-09-13 09:36:42 +0100

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
+import Header from './components/header/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="App" style={{ backgroundColor: '#0cbaba', backgroundImage: 'linear-gradient(315deg, #0cbaba 0%, #380036 100%)' }} >
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
