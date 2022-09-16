5580cb0a6730e502444c6b3d68800e8c43f73c80
Edswel <franckini@yahoo.com>
2022-09-16 05:52:52 +0100

    Add MessageBox for error handling

--- src/Screens/home/Home.jsx
+++ src/Screens/home/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { MenuItem, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, MenuItem, TextField } from '@mui/material';
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
+import MessageBox from '../../components/messageBox/MessageBox';
 
-function Home() {
+function Home({ fetchQuestions }) {
+    const [category, setCategory] = useState('');
+    const [error, setError] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        if (!category) {
+            setError(true);
+        } else {
+            setError(false);
+            // fetchQuestions(category);
+            navigate('/quiz');
+        }
+    }
+
     const categories = [
         {
             value: 'General Knowledge',
@@ -27,16 +43,19 @@ function Home() {
             <div className='Home-settings'>
                 <span style={{ fontSize: 30 }}>Settings</span>
                 <div className='Home-options'>
+                    {error && <MessageBox>Kindly fill out all required fields.</MessageBox>}
                     <TextField
                         label='Username'
                         variant='standard'
-                        style={{ marginBottom: 25 }}
+                    // style={{ marginBottom: 25 }}
                     />
                     <TextField
                         select
                         label="Categories"
                         variant="standard"
-                        style={{ marginBottom: 25 }}
+                        style={{ marginBottom: 30 }}
+                        onChange={(e) => setCategory(e.target.value)}
+                        value={category}
                     >
                         {categories.map((option) => (
                             <MenuItem key={option.value} value={option.value}>
@@ -44,6 +63,7 @@ function Home() {
                             </MenuItem>
                         ))}
                     </TextField>
+                    <Button variant='contained' size='large' onClick={handleSubmit}>Start Quiz</Button>
                 </div>
             </div>
             <img src='../../question.jpg' className='Home-image' alt="quiz image" />
---
+++ src/components/messageBox/MessageBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MessageBox.css';
+
+function MessageBox() {
+    return (
+        <div className='Container'>Kindly fill out all required fields.</div>
+    )
+}
+
+export default MessageBox;
